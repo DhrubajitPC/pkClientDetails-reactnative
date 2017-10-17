56e9840a8f5a01d9104acb33ddedbdd50236cb20
Dhrubajit <dhrubajit1992@gmail.com>
2017-10-17 12:06:42 +0800

    add react-native client side code

---
+++ client-details/pages/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text> Welcome to the home page </Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
