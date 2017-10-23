f8a8e3a40c479564854d5aaaed7fa3308c67a7ff
Dhrubajit <dhrubajit1992@gmail.com>
2017-10-24 06:53:40 +0800

    add Root component to app.js to enable toasts

--- client/App.js
+++ client/App.js
@@ -7,31 +7,34 @@ import {
 } from 'react-native';
 
 import { Stack, Actions, ActionConst, Scene, Router } from 'react-native-router-flux';
-
+import { Root } from 'native-base';
 import Home from './pages/home';
 import Login from './pages/login';
 
 export default class App extends Component {
+
   componentDidMount(){
     Actions.home();
   }
-
+  
   render() {
     return (
-      <Router>
-        <Stack key='root'>
-          <Scene 
-            key='login'
-            component={Login}
-            title='Login'
+      <Root>
+        <Router>
+          <Stack key='root'>
+            <Scene 
+              key='login'
+              component={Login}
+              title='Login'
+              />
+            <Scene 
+              key='home'
+              component={Home}
+              title='Client Details'
             />
-          <Scene 
-            key='home'
-            component={Home}
-            title='Client Details'
-          />
-        </Stack>
-      </Router>
+          </Stack>
+        </Router>
+      </Root>
     );
   }
 }
