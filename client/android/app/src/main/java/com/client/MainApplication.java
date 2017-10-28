f75ae0e7b1e39ed2d3c324c034718b50fb4d78e8
Dhrubajit <dhrubajit1992@gmail.com>
2017-10-29 03:04:04 +0800

    add react-native-exit-app module

--- client/android/app/src/main/java/com/client/MainApplication.java
+++ client/android/app/src/main/java/com/client/MainApplication.java
@@ -3,6 +3,7 @@ package com.client;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wumke.RNExitApp.RNExitAppPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNExitAppPackage(),
             new RCTCameraPackage(),
           new RNGoogleSigninPackage(),
           new LocationServicesDialogBoxPackage()
