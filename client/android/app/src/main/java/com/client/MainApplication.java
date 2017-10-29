2edab4f3fcb22abffdf8a3c2b0ac789b4fcc7d76
Dhrubajit <dhrubajit1992@gmail.com>
2017-10-29 13:27:51 +0800

    add react-native-file-system library

--- client/android/app/src/main/java/com/client/MainApplication.java
+++ client/android/app/src/main/java/com/client/MainApplication.java
@@ -3,6 +3,7 @@ package com.client;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.github.wumke.RNExitApp.RNExitAppPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RNExitAppPackage(),
             new RCTCameraPackage(),
           new RNGoogleSigninPackage(),
